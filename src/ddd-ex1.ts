a4d4d3cfdc010c7756542c786a5eb1ab34f9ac6d
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2020-04-24 22:49:16 +0900

    dev

---
+++ src/ddd-ex1.ts
@@ -0,0 +1,23 @@
+const barth = new Date(1986, 5, 22);
+
+class DateOfBarth {
+  private readonly YEARS_DATE_TIME = 1000 * 60 * 60 * 24 * 365;
+  private readonly CURRENT_DATE_TIME = new Date().getTime();
+  private readonly CURRENT_MONTH = new Date().getMonth() + 1;
+
+  constructor(private readonly barthDate: Date) {}
+
+  getAge(): number {
+    const yearsMilliSeconds = this.CURRENT_DATE_TIME - this.barthDate.getTime();
+    return Math.floor(yearsMilliSeconds / this.YEARS_DATE_TIME);
+  }
+
+  checkBarthMonth(): boolean {
+    return this.barthDate.getMonth() === this.CURRENT_MONTH;
+  }
+}
+
+const dateOfBarth = new DateOfBarth(barth);
+
+console.log(dateOfBarth.getAge());
+console.log(dateOfBarth.checkBarthMonth());
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { EcApp } from "./ec-app";
+import "./ddd-ex1";
 
 const ecApp = new EcApp();
 ecApp.serve();
